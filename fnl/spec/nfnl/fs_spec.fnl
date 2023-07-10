055eb3abfb4166b1f4fb228ea7668b785b75cbe0
Oliver Caldwell <ollie@oli.me.uk>
2023-07-10 11:10:00 +0100

    Add a couple of FS tests

--- fnl/spec/nfnl/fs_spec.fnl
+++ fnl/spec/nfnl/fs_spec.fnl
@@ -1,5 +1,19 @@
-; (deftest basename
-;   (t.= "foo" (fs.basename "foo/bar.fnl")))
+(local {: describe : it} (require :plenary.busted))
+(local assert (require :luassert.assert))
+(local fs (require :nfnl.fs))
 
-; (deftest path-sep
-;   (t.= "/" fs.path-sep))
+(describe
+  "basename"
+  (fn []
+    (it "removes the last segment of a path"
+        (fn []
+          (assert.equals "foo" (fs.basename "foo/bar.fnl"))
+          (assert.equals "foo/bar" (fs.basename "foo/bar/baz.fnl"))
+          (assert.equals "." (fs.basename "baz.fnl"))))))
+
+(describe
+  "path-sep"
+  (fn []
+    (it "returns the OS path separator (test assumes Linux)"
+        (fn []
+          (assert.equals "/" (fs.path-sep))))))
--- lua/spec/nfnl/fs_spec.lua
+++ lua/spec/nfnl/fs_spec.lua
@@ -1,2 +1,22 @@
 -- [nfnl] Compiled from fnl/spec/nfnl/fs_spec.fnl by https://github.com/Olical/nfnl, do not edit.
-
+local _local_1_ = require("plenary.busted")
+local describe = _local_1_["describe"]
+local it = _local_1_["it"]
+local assert = require("luassert.assert")
+local fs = require("nfnl.fs")
+local function _2_()
+  local function _3_()
+    assert.equals("foo", fs.basename("foo/bar.fnl"))
+    assert.equals("foo/bar", fs.basename("foo/bar/baz.fnl"))
+    return assert.equals(".", fs.basename("baz.fnl"))
+  end
+  return it("removes the last segment of a path", _3_)
+end
+describe("basename", _2_)
+local function _4_()
+  local function _5_()
+    return assert.equals("/", fs["path-sep"]())
+  end
+  return it("returns the OS path separator (test assumes Linux)", _5_)
+end
+return describe("path-sep", _4_)
