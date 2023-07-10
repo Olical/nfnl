6859fd5bce4ba112c5e0292f7080dbd44bd8ff00
Oliver Caldwell <ollie@oli.me.uk>
2023-07-10 11:06:58 +0100

    Add some macro tests

--- fnl/spec/nfnl/macros_spec.fnl
+++ fnl/spec/nfnl/macros_spec.fnl
@@ -1,34 +1,55 @@
-; (deftest if-let
-;   (t.= :ok
-;        (if-let [foo :ok]
-;          foo
-;          :nope))
-;   (t.= :nope
-;        (if-let [foo false]
-;          :first
-;          :nope))
-;   (t.= :yes
-;        (if-let [{: a} {:a 1}]
-;          :yes
-;          :no))
-;   (t.= :no
-;        (if-let [{: a} nil]
-;          :yes
-;          :no)))
+(local {: describe : it} (require :plenary.busted))
+(local assert (require :luassert.assert))
+(import-macros {: if-let : when-let} :nfnl.macros)
 
-; (deftest when-let
-;   (t.= :ok
-;        (when-let [foo :ok]
-;          foo))
-;   (t.= nil
-;        (when-let [foo false]
-;          :first))
-;   (t.= :yarp
-;        (when-let [(ok? val) (pcall #:yarp)]
-;          val))
-;   (t.= :yarp
-;        (when-let [(_ val) (pcall #:yarp)]
-;          val))
-;   (t.= nil
-;        (when-let [(ok? val) (pcall #(error :narp))]
-;          val)))
+(describe
+  "if-let"
+  (fn []
+    (it "works like if + let"
+        (fn []
+          (assert.equals
+            :ok
+               (if-let [foo :ok]
+                 foo
+                 :nope))
+          (assert.equals
+            :nope
+               (if-let [foo false]
+                 :first
+                 :nope))
+          (assert.equals
+            :yes
+               (if-let [{: a} {:a 1}]
+                 :yes
+                 :no))
+          (assert.equals
+            :no
+               (if-let [{: a} nil]
+                 :yes
+                 :no))))))
+
+(describe
+  "when-let"
+  (fn []
+    (it "works like when + let"
+        (fn []
+          (assert.equals
+            :ok
+            (when-let [foo :ok]
+              foo))
+          (assert.equals
+            nil
+            (when-let [foo false]
+              :first))
+          (assert.equals
+            :yarp
+            (when-let [(ok? val) (pcall #:yarp)]
+              val))
+          (assert.equals
+            :yarp
+            (when-let [(_ val) (pcall #:yarp)]
+              val))
+          (assert.equals
+            nil
+            (when-let [(ok? val) (pcall #(error :narp))]
+              val))))))
--- lua/spec/nfnl/macros_spec.lua
+++ lua/spec/nfnl/macros_spec.lua
@@ -1,2 +1,114 @@
 -- [nfnl] Compiled from fnl/spec/nfnl/macros_spec.fnl by https://github.com/Olical/nfnl, do not edit.
-
+local _local_1_ = require("plenary.busted")
+local describe = _local_1_["describe"]
+local it = _local_1_["it"]
+local assert = require("luassert.assert")
+local function _2_()
+  local function _3_()
+    local function _4_()
+      local foo = "ok"
+      if foo then
+        return foo
+      else
+        return "nope"
+      end
+    end
+    assert.equals("ok", _4_())
+    local function _6_()
+      local foo = false
+      if foo then
+        return "first"
+      else
+        return "nope"
+      end
+    end
+    assert.equals("nope", _6_())
+    local function _9_()
+      local value_6_auto = {a = 1}
+      local _let_8_ = (value_6_auto or {})
+      local a = _let_8_["a"]
+      if value_6_auto then
+        return "yes"
+      else
+        return "no"
+      end
+    end
+    assert.equals("yes", _9_())
+    local function _12_()
+      local value_6_auto = nil
+      local _let_11_ = (value_6_auto or {})
+      local a = _let_11_["a"]
+      if value_6_auto then
+        return "yes"
+      else
+        return "no"
+      end
+    end
+    return assert.equals("no", _12_())
+  end
+  return it("works like if + let", _3_)
+end
+describe("if-let", _2_)
+local function _14_()
+  local function _15_()
+    local function _16_()
+      local foo = "ok"
+      if foo then
+        return foo
+      else
+        return nil
+      end
+    end
+    assert.equals("ok", _16_())
+    local function _18_()
+      local foo = false
+      if foo then
+        return "first"
+      else
+        return nil
+      end
+    end
+    assert.equals(nil, _18_())
+    local function _21_()
+      local ok_3f, val = nil, nil
+      local function _20_()
+        return "yarp"
+      end
+      ok_3f, val = pcall(_20_)
+      if ok_3f then
+        return val
+      else
+        return nil
+      end
+    end
+    assert.equals("yarp", _21_())
+    local function _25_()
+      local bind_23_, val = nil, nil
+      local function _24_()
+        return "yarp"
+      end
+      bind_23_, val = pcall(_24_)
+      if bind_23_ then
+        return val
+      else
+        return nil
+      end
+    end
+    assert.equals("yarp", _25_())
+    local function _28_()
+      local ok_3f, val = nil, nil
+      local function _27_()
+        return error("narp")
+      end
+      ok_3f, val = pcall(_27_)
+      if ok_3f then
+        return val
+      else
+        return nil
+      end
+    end
+    return assert.equals(nil, _28_())
+  end
+  return it("works like when + let", _15_)
+end
+return describe("when-let", _14_)
