f233af894481a5576ab0432acf152bc572aa6dd1
Oliver Caldwell <ollie@oli.me.uk>
2023-12-07 10:35:18 +0000

    Move mappings / project config to .nvim.fnl

---
+++ .nvim.fnl
@@ -0,0 +1,19 @@
+(local {: autoload} (require :nfnl.module))
+(local reload (autoload :plenary.reload))
+(local notify (autoload :nfnl.notify))
+
+(vim.api.nvim_set_keymap
+  :n "<localleader>pt" "<Plug>PlenaryTestFile" {:desc "Run the current test file with plenary."})
+
+(vim.api.nvim_set_keymap
+  :n "<localleader>pT" "<cmd>PlenaryBustedDirectory lua/spec/<cr>" {:desc "Run all tests with plenary."})
+
+(vim.api.nvim_set_keymap
+  :n "<localleader>pr" ""
+  {:desc "Reload the nfnl modules."
+   :callback (fn []
+               (notify.info "Reloading...")
+               (reload.reload_module "nfnl")
+               (require :nfnl)
+               (notify.info "Done!"))})
+
---
+++ .nvim.lua
@@ -0,0 +1,14 @@
+-- [nfnl] Compiled from .nvim.fnl by https://github.com/Olical/nfnl, do not edit.
+local _local_1_ = require("nfnl.module")
+local autoload = _local_1_["autoload"]
+local reload = autoload("plenary.reload")
+local notify = autoload("nfnl.notify")
+vim.api.nvim_set_keymap("n", "<localleader>pt", "<Plug>PlenaryTestFile", {desc = "Run the current test file with plenary."})
+vim.api.nvim_set_keymap("n", "<localleader>pT", "<cmd>PlenaryBustedDirectory lua/spec/<cr>", {desc = "Run all tests with plenary."})
+local function _2_()
+  notify.info("Reloading...")
+  reload.reload_module("nfnl")
+  require("nfnl")
+  return notify.info("Done!")
+end
+return vim.api.nvim_set_keymap("n", "<localleader>pr", "", {desc = "Reload the nfnl modules.", callback = _2_})
