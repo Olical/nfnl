f3b9d4e0daf4765b730f600b58e5dcc2f1a2cbbe
Kaylee Simmons <kay@the-simmons.net>
2024-06-21 15:44:55 -0700

    More progress on figuring out where the test failure is happening. I suspect its upstream

--- script/test.ps1
+++ script/test.ps1
@@ -1,4 +1,7 @@
 $env:XDG_CONFIG_HOME = "$PWD/.test-config"
+$env:XDG_STATE_HOME = "$PWD/.test-state"
 
-nvim --headless -c 'edit .nfnl.fnl' -c trust -c qa -i NONE
-nvim --headless -c 'PlenaryBustedDirectory lua/spec' -i NONE
+rm -for -rec $env:XDG_STATE_HOME -erroraction 'silentlycontinue'
+
+nvim --headless -c 'edit .nfnl.fnl' -c trust -c qa
+nvim --headless -c 'PlenaryBustedDirectory lua\\spec'
