d6db4cfda10932a61e35f5afd4dde02ca089e60e
Oliver Caldwell <ollie@oli.me.uk>
2024-07-26 16:13:30 +0100

    Print fileformat / line ending info

--- script/test.ps1
+++ script/test.ps1
@@ -3,5 +3,5 @@ $env:XDG_STATE_HOME = "$PWD/.test-config/state"
 
 rm -for -rec $env:XDG_STATE_HOME -erroraction 'silentlycontinue'
 
-nvim --headless -c 'edit .nfnl.fnl' -c trust -c qa
+nvim --headless -c 'edit .nfnl.fnl' -c 'set fileformat?' -c 'set fixeol?' -c 'echo &modified' -c trust -c qa
 nvim --headless -c 'PlenaryBustedDirectory lua\\spec'
