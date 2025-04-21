71aa3ee95363adc6d75ff063ad12b77c936dc3a2
Oliver Caldwell <ollie@oli.me.uk>
2025-04-21 12:08:08 +0100

    Add mostly empty gc module

---
+++ fnl/nfnl/gc.fnl
@@ -0,0 +1,6 @@
+(local {: autoload : define} (require :nfnl.module))
+(local fs (autoload :nfnl.fs))
+
+(local M (define :nfnl.gc))
+
+M
---
+++ lua/nfnl/gc.lua
@@ -0,0 +1,7 @@
+-- [nfnl] fnl/nfnl/gc.fnl
+local _local_1_ = require("nfnl.module")
+local autoload = _local_1_["autoload"]
+local define = _local_1_["define"]
+local fs = autoload("nfnl.fs")
+local M = define("nfnl.gc")
+return M
